ea0287980010db02d1971d79ab25183975ca6af1
YogaAlfaRisqi <yogaalfarisqi4@gmail.com>
2026-06-20 11:06:21 +0700

    feat: add feature crud alternative

---
+++ app/Models/AlternativeValue.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class AlternativeValue extends Model
+{
+    protected $fillable = ['alternative_id', 'criteria_id', 'value'];
+ 
+    protected function casts(): array
+    {
+        return ['value' => 'decimal:2'];
+    }
+ 
+    /**
+     * @return BelongsTo<Alternative, AlternativeValue>
+     */
+    public function alternative(): BelongsTo
+    {
+        return $this->belongsTo(Alternative::class);
+    }
+ 
+    /**
+     * @return BelongsTo<Criteria, AlternativeValue>
+     */
+    public function criteria(): BelongsTo
+    {
+        return $this->belongsTo(Criteria::class);
+    }
+}
